c6e3a0576091261fcf0b29fd75f2305629ed087b
AlanAnto <alanantojoy007@gmail.com>
2022-12-15 17:56:31 +0530

    Add project files.

---
+++ MyFood/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿namespace MyFood.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+
+        }
+        public DbSet<ApplicationUser> Users { get; set; }
+
+        public DbSet<Food> Foods { get; set; }
+
+        public DbSet<FoodOrder> FoodOrders { get; set; }
+
+        public DbSet<Location> Locations { get; set; }
+        
+        public DbSet<Order> Orders { get; set; }
+
+
+    }
+}
---
+++ MyFood/Models/FoodOrder.cs
@@ -0,0 +1,25 @@
+﻿namespace MyFood.Models
+{
+    public class FoodOrder
+    {
+        public int Id { get; set; }
+
+        public Order ItemOrder { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(ItemOrder))]
+        public int OrderId { get; set; }
+
+        public Food Food { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Food))]
+        public int FoodId { get; set;}
+
+        [Required]
+        public int Quantity { get; set; } = 1;
+
+        [Required]
+        public double Amount { get; set; }
+    }
+}
